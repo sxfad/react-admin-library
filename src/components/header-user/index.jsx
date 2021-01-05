a9a48302433300985e850fe168987dd08f7474f6
zkboys <876213774@qq.com>
2021-01-05 16:42:41 +0800

    :art: 整理代码

--- src/components/header-user/index.jsx
+++ src/components/header-user/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { CaretDownOutlined } from '@ant-design/icons';
 import { Dropdown, Menu } from 'antd';
 import UserLink from '../user-link';
@@ -7,9 +8,17 @@ import styles from './style.module.less';
 const Item = Menu.Item;
 
 export default class HeaderUser extends Component {
+    static propTypes = {
+        theme: PropTypes.string,
+        loginUser: PropTypes.object,
+        showAvatar: PropTypes.bool,
+        menus: PropTypes.array,
+    };
+
     static defaultProps = {
         theme: 'default',
         loginUser: {},
+        showAvatar: true,
     };
 
     state = {};
@@ -17,13 +26,13 @@ export default class HeaderUser extends Component {
     render() {
         const user = this.props.loginUser || {};
 
-        const { className, menus, theme } = this.props;
+        const { className, menus, theme, showAvatar } = this.props;
 
         const menu = (
             <Menu className={styles.menu} theme={theme} selectedKeys={[]}>
                 {menus.map((item, index) => {
                     const { text, onClick } = item;
-                    if (text === 'Divider') return <Menu.Divider/>;
+                    if (text === 'Divider') return <Menu.Divider />;
 
                     return <Item key={index} onClick={onClick}>{text}</Item>;
                 })}
@@ -33,11 +42,11 @@ export default class HeaderUser extends Component {
         return (
             <div className={styles.userMenu} ref={node => this.userMenu = node}>
                 <Dropdown trigger="click" overlay={menu} getPopupContainer={() => (this.userMenu || document.body)}>
-                    <span className={[ styles.account, className ].join(' ')}>
+                    <span className={[styles.account, className].join(' ')}>
                         <span className={styles.userName}>
-                            <UserLink user={user} link={false}/>
+                            <UserLink user={user} showAvatar={showAvatar} link={false} />
                         </span>
-                        <CaretDownOutlined/>
+                        <CaretDownOutlined />
                     </span>
                 </Dropdown>
             </div>
